e23c25ea4707859278452636c9230b86fe155cf8
Silenat Teklebirehan Hailu <870061541@edu.taitotalo.fi>
2026-09-21 10:37:02 +0300

    codi lisätyy

--- shop.py
+++ shop.py
@@ -1,1 +1,15 @@
-print("shop is created")
+def open_shop():
+    print("welcom to the shop!")
+    print("1. ostaa miekan: 14 euroa")
+    print("2. ostaa tacki: 140 euroa")
+
+    choice= input("mistä haluat ostamaan?")
+    if (choice== "1"):
+        print("sinä ostat miekan")
+        return "miekan"
+    elif (choice=="2"):
+        print("sinä ostat tacki")
+        return "taki"
+    else:
+        print("et valitse mitään")
+        return None
