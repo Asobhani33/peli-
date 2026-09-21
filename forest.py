381b3ea4503d634ba419f9851c2a4666005fd4f1
Mamur <802041285@edu.taitotalo.fi>
2026-09-21 10:57:45 +0300

    Something added

--- forest.py
+++ forest.py
@@ -1,1 +1,10 @@
-print('Hello Python')
+def enter_forest():
+    print('You enter the forest')
+    print('A very big troll appears front of you')
+
+    choice = input('Do you want to fight? (yes/no):')
+
+    if(choice == 'yes'):
+        print('You fight the troll and your ass is kicked')
+    else:
+        print('You escape back to village.')
