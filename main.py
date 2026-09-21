eb4fffc9766031052039cb50c772563892aff6fa
Ali <870041062@edu.taitotalo.fi>
2026-09-21 11:32:07 +0300

    I update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello wolrd")
+from forest import enter_forest
+from shop import open_shop
+
+
+choise =  input("choose:)")
+
+if (choise == "1"):
+   enter_forest()
+elif(choise =="2"):
+   open_shop()
